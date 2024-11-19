cbe6e02cca53c1ce0fdf7330eaab5fd78bd64b74
Mohamed Soltan <soltan.mohamed@esprit.tn>
2024-11-19 15:06:30 +0100

    version 1

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\FoyerRepository;
+use App\Repository\ChambreRepository; // Ajoutez le repository des chambres
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function index(): Response
+    public function index(FoyerRepository $foyerRepository, ChambreRepository $chambreRepository): Response
     {
-        return $this->render('dashboard.html.twig');
+        // Récupérer tous les foyers et leurs chambres
+        $foyers = $foyerRepository->findAll();
+
+        // Récupérer toutes les chambres
+        $chambres = $chambreRepository->findAll();
+
+        return $this->render('dashboard.html.twig', [
+            'foyers' => $foyers,
+            'chambres' => $chambres, // Passez les chambres à la vue
+        ]);
     }
 }
